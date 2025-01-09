cc296755d2d4ed11a71a043cbba489de52d2d6fe
Umar <umarameen026@gmail.com>
2025-01-09 13:15:40 +0530

    modified Youtube.tsx

--- app/youtube.tsx
+++ app/youtube.tsx
@@ -30,10 +30,13 @@ export default function Youtube({
       <Link href="video">
         <div className="relative">
           <img src={thumbnailUrl} className="block w-full" />
-          <div className="absolute bottom-6 right-2 bg-secondary-dark text-secondary text-sm px-0.5 rounded text-red-600">{duration}</div>
+          <div className="absolute bottom-2 right-2 bg-secondary-dark text-secondary text-sm px-0.5 rounded text-red-600">
+            {duration}
+          </div>
         </div>
         <p className="text-blue-600 font-bold">{title}</p>
       </Link>
+
       <div className="flex gap-2">
         <img src={channel.profileUrl} className=" w-12 h-12 rounded-full" />
         <p className="text-secondary-text text-sm">{channel.name}</p>
@@ -41,3 +44,4 @@ export default function Youtube({
     </div>
   );
 }
+
